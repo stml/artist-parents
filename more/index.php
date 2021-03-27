c5f7c843d474a7e60757972e9cb1617f2c0a245b
stml <james@shorttermmemoryloss.com>
2021-03-27 12:12:08 +0200

    More info page

--- more/index.php
+++ more/index.php
@@ -47,10 +47,25 @@
       <div class="row links">
         <div class="column">
 
-          <p>TBC</p>
+          <p>‘How Not To Exclude Artist Parents: Some Guidelines for Institutions and Residencies’ was written by Hettie Judah and a group of artist mothers, among them Catherine Kurtz, Cecile Johnson Soliz, Chloe Bonfield, Elsa James, Emma Franks, Emma Hart, Emma Talbot, Eva Rothschild, Gaia Fugazza, Helen Benigson, Holly Blakey, Ingrid Berthon-Moine, Joanne Masding, Kate V. Robertson, Laima Leyton, Laura Ford, Macarena Rojas, Magda Bielesz, Maureen Nathan, Melanie Jackson, Naomi Frears, Rana Begum, Rosalind Faram, Sarah Boulton, and Selina Ogilvy.</p>
+
+          <p>The guidelines were presented during the online discussion <a href="https://freelandsfoundation.co.uk/event/how-not-to-exclude-artist-mothers-a-conversation-about-artists-parenting-and-institutions">How Not To Exclude Artist Mothers</a> hosted by The Freelands Foundation, London, on 4 March 2021. The roots of this project lie in Hettie’s earlier research into the impact of motherhood on artists’ careers, an essay on which was commissioned by Dr Kate MacMillan as part of her 2019 <a href="https://freelandsfoundation.imgix.net/documents/Representation-of-female-artists-2019-Clickable.pdf">Representation of Female Artists in Britain</a> report.</p>
+
+          <p>With thanks to Hannah Bowles for the layout of the original guidelines, to Laima Leyton for translating them into Portuguese, Ingrid Berthon-Moine into French, Magda Bielesz in Polish.</p>
+
+          <p>And to James Bridle for creating this website.</p>
 
         </div>
       </div>
+
+
+            <div id="footer">
+            <div class="row">
+              <div class="column">
+                <p><a href="..">Home</a></p>
+              </div>
+            </div>
+          </div>
 
 </div>
 
